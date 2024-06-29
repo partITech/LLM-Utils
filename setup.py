2e7ce2bf9e15864f1d5706a6ecdcf8b6a3ca5fa3
geraud <gtwin@bourdin.name>
2024-06-29 20:48:15 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="llm_tools",
+    version="0.0.1",
+    author="Géraud Bourdin, Thomas Bourdin",
+    author_email="gbourdin@partitech.com, tbourdin@partitech.com",
+    description="Une collection d'outils utiles pour l'entraînement de grands modèles de langage.",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'datasets',
+        'mistral_common',
+        'transformers',
+        'PyTorch'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
